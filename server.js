fefe25aed2cece9f7725ab6d001b961edf215b3d
thapaswini09 <thapaswini.vanukuri@gmail.com>
2025-08-31 17:13:49 +0530

    Update CORS to allow frontend deployment URL

--- server.js
+++ server.js
@@ -1,23 +1,26 @@
-const express=require('express');
-const dbConnetion = require('./dbConnection/dbConnection');
-const cors=require('cors')
-const cookieParser = require('cookie-parser');
-const router=require('./Router/router')
-const path = require('path');
-const app=express();
-const dotenv=require('dotenv').config();
+const express = require("express");
+const dbConnetion = require("./dbConnection/dbConnection");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const router = require("./Router/router");
+const path = require("path");
+const app = express();
+const dotenv = require("dotenv").config();
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({
-    // origin: '*'
-   origin: 'http://localhost:3001', // your React frontend
-  credentials: true 
-   
-  }));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3001",
+      "https://foodorderingfrontend-2mix.onrender.com",
+    ],
+    credentials: true,
+  })
+);
 
-dbConnetion()
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
-app.use('/api',router)
+dbConnetion();
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+app.use("/api", router);
 
-const PORT=process.env.PORT;
-app.listen(PORT,()=>console.log(`server is running on ${PORT}`));
+const PORT = process.env.PORT;
+app.listen(PORT, () => console.log(`server is running on ${PORT}`));
